3264e57e9c6b6e241852b7d49bf1176651d22147
canbekcan <can@canbekcan.com>
2026-04-12 17:51:20 +0300

    v1.0.2

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,18 @@
+# name: discourse-localized-badges
+# about: Custom badges localisation for Discourse
+# version: 0.1
+# authors: Senior Engineer
+# url: https://github.com/canbekcan/discourse-localized-badges
+
 # frozen_string_literal: true
 
 enabled_site_setting :localized_badges_enabled
 
 after_initialize do
-  # Eklenti kapalıysa işlem yapma
   next unless SiteSetting.localized_badges_enabled
 
   reloadable_patch do
     
-    # 1. API YAMASI: Badge Serializer (Rozetler sayfası ve listeler için)
     module ::LocalizedBadgeSerializerPatch
       def name
         object.name.to_s.start_with?('badges.') ? I18n.t(object.name) : super
@@ -24,7 +28,6 @@ after_initialize do
       prepend ::LocalizedBadgeSerializerPatch
     end
 
-    # 2. API YAMASI: User Badge Serializer (Kullanıcı profilindeki rozetler için)
     module ::LocalizedUserBadgeSerializerPatch
       def badge_name
         object.badge.name.to_s.start_with?('badges.') ? I18n.t(object.badge.name) : super
@@ -36,7 +39,6 @@ after_initialize do
       prepend ::LocalizedUserBadgeSerializerPatch
     end
 
-    # 3. MODEL YAMASI: Sunucu içi işlemler, bildirimler ve e-postalar için
     module ::LocalizedBadgeModelPatch
       def display_name
         name.to_s.start_with?('badges.') ? I18n.t(name) : super
