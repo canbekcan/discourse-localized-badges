8bc8d3ca88b44ab7fad108cc16e1fd91621014ed
canbekcan <can@canbekcan.com>
2026-08-03 03:07:42 +0300

    Badge information settings added into migration

--- db/post_migrate/20260802000000_seed_sponsor_badges.rb
+++ db/post_migrate/20260802000000_seed_sponsor_badges.rb
@@ -11,8 +11,10 @@ class SeedSponsorBadges < ActiveRecord::Migration[7.0]
 
     badges.each do |b|
       Badge.find_or_initialize_by(name: b[:name]).tap do |badge|
-        badge.badge_type_id = 1 # Gold/Bronze/Silver seviyeleri rozet tipine göre (1, 2, 3) ayarlanabilir.
+        badge.badge_type_id = 1 
         badge.icon = b[:icon]
+        badge.description = "badges.#{b[:key]}.description"
+        badge.long_description = "badges.#{b[:key]}.long_description"
         badge.allow_title = true
         badge.system = true
         badge.save!
