637b99a7b2befc2dd130dcc00ad0fe40ba937096
canbekcan <can@canbekcan.com>
2026-09-10 00:04:38 +0300

    fixed 2

--- db/post_migrate/20260909000000_seed_verified_publisher_badge.rb
+++ db/post_migrate/20260909000000_seed_verified_publisher_badge.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-Badge.find_or_create_by!(name: 'Verified Publisher') do |badge|
-  badge.description = 'Awarded to users registered with verified publisher domains.'
-  badge.badge_type_id = 3 # 1: Altın, 2: Gümüş, 3: Bronz
-  badge.system = true
-  badge.allow_title = true
+class SeedVerifiedPublisherBadge < ActiveRecord::Migration[7.0]
+  def up
+    Badge.find_or_create_by!(name: 'Verified Publisher') do |badge|
+      badge.description = 'Awarded to users registered with verified publisher domains.'
+      badge.badge_type_id = 3 # 1: Altın, 2: Gümüş, 3: Bronz
+      badge.system = true
+      badge.allow_title = true
+    end
+  end
+
+  def down
+    Badge.find_by(name: 'Verified Publisher')&.destroy
+  end
 end
