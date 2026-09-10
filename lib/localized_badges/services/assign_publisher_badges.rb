c8f01c7753b94e6df6f67ccfb364201d741efe5f
canbekcan <can@canbekcan.com>
2026-09-10 16:17:42 +0300

    Assign the group

--- lib/localized_badges/services/assign_publisher_badges.rb
+++ lib/localized_badges/services/assign_publisher_badges.rb
@@ -14,26 +14,43 @@ module LocalizedBadges
         # Eşzamanlı işlemlerde veritabanı çakışmalarını önlemek için kilit (lock)
         DistributedMutex.synchronize("assign_publisher_badge_#{@user.id}") do
           ActiveRecord::Base.transaction do
-            check_and_manage('Verified Publisher', SiteSetting.localized_badges_publisher_domains)
+            check_and_manage(
+              'Verified Publisher', 
+              SiteSetting.localized_badges_publisher_domains, 
+              SiteSetting.publisher_target_groups
+            )
           end
         end
       end
 
       private
 
-      def check_and_manage(badge_name, domains_setting)
+      def check_and_manage(badge_name, domains_setting, groups_setting)
         badge = Badge.find_by(name: badge_name)
         return unless badge
 
-        # Ayar tamamen boşsa bile return etme. Array'e çevir ki aşağıdaki 'else' bloğu çalışıp rozeti geri alabilsin.
         domain_list = domains_setting.to_s.split('|').reject(&:blank?).map(&:downcase)
+        target_groups = groups_setting.to_s.split('|').reject(&:blank?)
         
         if domain_list.include?(@domain)
+          # 1. Rozeti ver
           BadgeGranter.grant(badge, @user)
+
+          # 2. Tanımlı gruplara ekle
+          target_groups.each do |group_name_or_id|
+            group = Group.find_by(name: group_name_or_id) || Group.find_by(id: group_name_or_id)
+            group.add(@user) if group && !group.users.include?(@user)
+          end
         else
-          # Kullanıcıda rozet varsa ve domain listesinde artık yoksa rozeti anında geri al (revoke)
+          # 1. Kullanıcıda rozet varsa geri al (revoke)
           user_badge = UserBadge.find_by(user_id: @user.id, badge_id: badge.id)
           BadgeGranter.revoke(user_badge) if user_badge
+
+          # 2. Tanımlı gruplardan çıkar
+          target_groups.each do |group_name_or_id|
+            group = Group.find_by(name: group_name_or_id) || Group.find_by(id: group_name_or_id)
+            group.remove(@user) if group && group.users.include?(@user)
+          end
         end
       end
     end
